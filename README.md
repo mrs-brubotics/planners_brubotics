ab385c972e4686ba2d1dce213a4183477d4e4070
liu <you@example.com>
2021-03-18 15:51:21 +0100

    moveit path and mrs connection

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # planners_brubotics
+![alt text](https://github.com/mrs-brubotics/planners_brubotics/blob/main/.fig/background.jpg)
+## prerequisite
+* Install MRS
+* Install Moveit 1 - Melodic following [instruction](http://docs.ros.org/en/melodic/api/moveit_tutorials/html/doc/getting_started/getting_started.html)
+## Quike Test 
+This package is built based on the [mrs_uav_system](https://github.com/ctu-mrs/mrs_uav_system). 
+* Run mrs: go ~/mrs_workspace/src/simulation/example_tmux_scripts/one_drone_gps, run:
+```
+./start.sh
+```
+* The sconde step under planners_brubotics workspace, run:
+```
+source devel/setup.bash
+```
+```
+roslaunch moveit_planner execute.launch
+```
+
+
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/liu/planners_brubotics/devel:$CMAKE_PREFIX_PATH"
+export GAZEBO_MODEL_PATH="/home/liu/mrs_workspace/install/share/mavlink_sitl_gazebo/models:$GAZEBO_MODEL_PATH"
+export GAZEBO_PLUGIN_PATH="/home/liu/mrs_workspace/install/lib/mavlink_sitl_gazebo/plugins:$GAZEBO_PLUGIN_PATH"
+export GAZEBO_RESOURCE_PATH="/home/liu/mrs_workspace/install/share/mavlink_sitl_gazebo:$GAZEBO_RESOURCE_PATH"
+export LD_LIBRARY_PATH='/home/liu/workspace/devel/lib:/home/liu/mrs_workspace/devel/lib:/opt/ros/melodic/lib:/usr/lib/x86_64-linux-gnu/gazebo-9/plugins:/home/liu/mrs_workspace/install/lib/mavlink_sitl_gazebo/plugins:/home/liu/mrs_workspace/install/lib/mavlink_sitl_gazebo/plugins:/home/liu/mrs_workspace/install/lib/mavlink_sitl_gazebo/plugins'
+export PWD='/home/liu/planners_brubotics/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/liu/planners_brubotics/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/liu/planners_brubotics/src:$ROS_PACKAGE_PATH"
