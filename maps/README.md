9c992c566c91f6edbca49d69a94e45501d065973
liu <you@example.com>
2021-04-04 14:38:37 +0200

    add_objects

---
+++ maps/README.md
@@ -0,0 +1,13 @@
+# maps
+This folder contains the object and map files which will be used to test our planner, including:
+| Object      | Size             | Center          |
+|-------------|------------------|-----------------|
+| Wall        | 0.3 x 5.0 x 5.0  | (3.0, 0.0, 2.5) |
+* Trunk: a o.25 x 4 cylinder object
+* Simple_maze
+* Square_exit
+There are two ways to give planners the information of the environment:
+* Scene objects
+* Octomap
+
+
---
+++ scripts/add_objects.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+def main():
+    rospy.init_node('add_objects')
+    scene = moveit_commander.PlanningSceneInterface()
+    rospy.sleep(1)
+
+    box_pose = geometry_msgs.msg.PoseStamped()
+    box_pose.header.frame_id = "geometry_center"
+    box_pose.pose.orientation.w = 1.0
+    box_pose.pose.position.x = 3
+    box_pose.pose.position.y = 3
+    box_pose.pose.position.z = 0.5 
+    box_name = "box"
+    scene.add_box(box_name, box_pose, size=(1, 1, 1))
+
+    current_known_objects = scene.get_known_object_names()
+    timeout = 4
+    start = rospy.get_time()
+    while True:
+        current_known_objects = scene.get_known_object_names()
+        if box_name not in current_known_objects:
+            print ("Wait for the object to spawn.")
+            rospy.sleep(0.5)
+            seconds = rospy.get_time()
+            if seconds - start > timeout:
+                print "Add objects failed."
+                break
+
+        else:
+            #print(current_known_objects)
+            print "Add objects done!"
+            break
+
+    #tutorial.remove_box()
+    
+    #return True
+
+if __name__ == '__main__':
+    main()
