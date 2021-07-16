4bc526506ddf1c586d7c6cd12f297ca024f3cbb6
kefran88 <frank_mukwege@hotmail.com>
2021-07-16 13:10:13 +0200

    Commander now call trajectory_reference service instead of /goto

--- scripts/mrs_commander.py
+++ scripts/mrs_commander.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
+from numpy.core.numeric import False_
 import rospy
+from geometry_msgs.msg import Point 
 from moveit_msgs.msg import DisplayTrajectory
 from mrs_msgs.msg import UavState
+from mrs_msgs.msg import TrajectoryReference
+from mrs_msgs.msg import Reference
 from mrs_msgs.srv import Vec4
+from mrs_msgs.srv import TrajectoryReferenceSrv
 import numpy as np
+
 
 
 
@@ -17,16 +23,19 @@ class path_subscriber:
         # Points is a list
         self.got_it = True
         self.way_points = data.trajectory[0].joint_trajectory.points
+        #self.header = data.trajectory[0].joint_trajectory.header
         # Anytime a new massage arrives, the callback function will be trigered 
 
     def listener(self):
         #rospy.init_node('trajectory_listener', anonymous=False)
+        print("Listening Right Now !")
         rospy.Subscriber("/move_group/display_planned_path", DisplayTrajectory, self.callback)
 
 
 class mrs_utils:
     def __init__(self):
-        self.position_cmd_call = rospy.ServiceProxy('/uav1/control_manager/goto', Vec4)
+        self.position_cmd_call = rospy.ServiceProxy('/uav1/control_manager/goto', Vec4) #rospy.ServiceProxy('/uav1/control_manager/trajectory_reference', TrajectoryReferenceSrv)
+        self.traj = rospy.ServiceProxy('/uav1/control_manager/trajectory_reference',TrajectoryReferenceSrv)
         self.tolerable_error = 0.2
 
     def take_observation(self):
@@ -59,21 +68,39 @@ class mrs_utils:
             if dist < self.tolerable_error:
                 cmd_achieved = True
         return cmd_achieved
+    #def publish_trajectory(self,path):
+        #send_it = rospy.Publisher('/trajectory',TrajectoryReference, queue_size=10)
+        #args must be ['header', 'use_heading', 'fly_now', 'loop', 'dt', 'points']
+        #Ref = Reference(path,np.zeros(len(path)))
+        #print(Ref)
+        #send_it.publish(TrajectoryReference([rospy.Header, self.use_heading_, self.fly_now_, self.loop_,0.2 , path]))
+        #send_it.publish(TrajectoryReference([rospy.Header, self.use_heading_, self.fly_now_, self.loop_,0.2 , Ref]))
+        #self.traj(path)
+        #return True
+
+
 
 if __name__ == '__main__':
 
     rospy.init_node('mrs_commander')
-    rate = rospy.Rate(5) # ROS Rate at 5Hz
+    rate = rospy.Rate(20) # ROS Rate at 5Hz
 
     mrs = mrs_utils()
     path_sub = path_subscriber()
     path_sub.listener()
-
+    Ref = Reference()
+    Traj = TrajectoryReference()
+    Traj.use_heading = False
+    Traj.fly_now = True
+    Traj.loop = False
+    Traj.dt = 0.08
+    Traj.points = Reference()
     while not rospy.is_shutdown(): 
         if path_sub.got_it == True:
             print("Got a new path!")
             path_sub.listener()
             path = []
+            pos = []
             for i in path_sub.way_points:
                 path.append(i.positions)
             #for i in range(len(path_sub.way_points)):
@@ -81,16 +108,38 @@ if __name__ == '__main__':
                 #path.append(point)
             path_sub.got_it = False
             #print(path)
+            #mrs.publish_trajectory(path)
             # Send the command
-            for i in path:
-                command_position = list(i)
-                command_position.append(0.0)
+            for i in path:  
+                #command_position = list(i)
+                point = Point(i[0],i[1],i[2])
+                heading_i = 0.0
+                Ref = Reference(point,heading_i)
+                pos.append(Ref)
+                #command_position.append(0.0)
+                #t_position = Point(i[0],i[1],i[2])
+                #Ref = Reference(t_position,0.0)
+                #t_position
                 #print(command_position)
                 #data_pose = mrs.take_observation()
             
-                mrs.position_cmd_call(command_position)
-                cmd_achieved = mrs.cmd_achieve(command_position)
-
+                #mrs.position_cmd_call(command_position)
+                #cmd_achieved = mrs.cmd_achieve(command_position)
+            #Traj.points = Ref #ERROR : trajectory.points must be a list
+            #Traj.points = [list(Ref.position),Ref.heading]
+            Traj.points = pos
+            rospy.loginfo("For Loop Done !")
+            #print(Traj)
+            #Traj.points = [t_position,np.zeros(len(t_position))]
+            #print(type(Traj.points))
+            #pub = rospy.Publisher('uav1/control_manager/trajectory_reference',TrajectoryReference,queue_size=10)
+            #pub.publish(Traj)
+            rospy.loginfo("Published !")
+            #Traj.points.heading = np.zeros(len(t_position))
+            #Traj.points.heading = 0
+            #print(Traj)
+            mrs.traj(Traj)
+            rospy.loginfo("Service called !")
             #print(path)
         rate.sleep()
 
